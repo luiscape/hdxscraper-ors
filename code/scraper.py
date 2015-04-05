f11130abea5dc6a5922841662d1d0901b6c310c3
Luis Capelo <luiscape@gmail.com>
2015-04-05 15:35:45 -0400

    adding config for different tables and schemas

--- code/scraper.py
+++ code/scraper.py
@@ -14,8 +14,8 @@ from store_records import StoreRecords
 
 dir = os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]
 
-def FetchData():
-  u = 'http://ors.ocharowca.info/anonymous/alldatafeed.ashx?format=json'
+def FetchData(endpoint):
+  u = endpoint["url"]
   r = requests.get(u)
 
   if r.status_code != 200:
@@ -31,12 +31,14 @@ def ProcessRecords(data):
 
   for row in data:
     record = [{ key:row[key] if isinstance(row[key], dict) is False else row[key].values()[0] for key in row.keys() }]
-    StoreRecords(record, "ors_all_data")
+    StoreRecords(record, endpoint["table_name"])
 
 
 def Main():
-  json = FetchData()
-  ProcessRecords(data=json)
+  endpoints = Config.LoadEndpoints()
+  for endpoint in endpoints:
+    json = FetchData(endpoint)
+    ProcessRecords(data=json)
 
 if __name__ == '__main__':
   Main()
