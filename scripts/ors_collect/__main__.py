0d7de89aa7d6caf310991897439219d11b3f50a7
Luis Capelo <luiscape@gmail.com>
2015-05-18 11:01:39 -0400

    deploy to sw

--- scripts/ors_collect/__main__.py
+++ scripts/ors_collect/__main__.py
@@ -4,13 +4,14 @@
 import os
 import sys
 import time
+import scraperwiki
 
 dir = os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]
 sys.path.append(dir)
 
 from utilities.hdx_format import item
-from ors_collect import collect as Collect
 from ors_collect import patch as Patch
+from ors_collect import collect as Collect
 
 def Main(patch=True):
   '''Wrapper for main program.'''
@@ -27,15 +28,14 @@ def Main(patch=True):
     Patch.Main()
 
 if __name__ == '__main__':
-  Main()
+  # Main()
 
+  try:
+      Main()
+      print "SW Status: Everything seems to be just fine."
+      scraperwiki.status('ok')
 
-#   try:
-#       Main()
-#       print "SW Status: Everything seems to be just fine."
-#       scraperwiki.status('ok')
-
-#   except Exception as e:
-#       print e
-#       scraperwiki.status('error', 'Error collecting data.')
-#       os.system("echo https://ds-ec2.scraperwiki.com/3zarzzv/0zftw6fzkjxommp/http/log.txt | mail -s 'ORS APIs: Failed collecting data.' luiscape@gmail.com")
+  except Exception as e:
+      print e
+      scraperwiki.status('error', 'Error collecting data.')
+      os.system("echo https://ds-ec2.scraperwiki.com/3zarzzv/0zftw6fzkjxommp/http/log.txt | mail -s 'ORS APIs: Failed collecting data.' luiscape@gmail.com")
