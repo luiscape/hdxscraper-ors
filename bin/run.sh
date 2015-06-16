89468dc597e94df7ef8df9dc3ef3aed481ad0b42
Luis Capelo <luiscape@gmail.com>
2015-06-16 11:57:43 -0400

    adding makefile

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source venv/bin/activate
-python tool/scripts/ors_collect/ #> tool/http/log.txt
-python tool/scripts/hdx_datastore/ #> tool/http/datastore_log.txt
+python tool/scripts/ors_collect/
+python tool/scripts/hdx_datastore/
